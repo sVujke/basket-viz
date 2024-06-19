61b9ba182197d8cb33de9f7992e4a16a70d5decf
vujke <stefanvujovic93@gmail.com>
2024-06-19 17:02:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+basket-viz
+==============================
+
+A Python library for creating interactive and customizable visualizations of basketball statistics.
+
+
+# Shot Charts 
+
+![Alt Text](/media/shots.gif)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Python library for creating interactive and customizable visualizations of basketball statistics.',
+    author='basket-viz',
+    license='MIT',
+)
