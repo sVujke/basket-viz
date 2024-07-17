b7b1e183ceb45b13bc485f84ba557412a13f6204
vujke <stefanvujovic93@gmail.com>
2024-07-17 23:45:00 +0200

    added setter, getter, dynamic parametrization

--- basket_viz/overlay/score_trajectory.py
+++ basket_viz/overlay/score_trajectory.py
@@ -12,8 +12,8 @@ from scipy.interpolate import make_interp_spline
 # # )
 
 # # # Filter out players that appear less than 200 times
-# # player_counts = df["PLAYER"].value_counts()
-# # df = df[df["PLAYER"].isin(player_counts[player_counts >= 200].index)]
+# # player_counts = df[self.subject_col].value_counts()
+# # df = df[df[self.subject_col].isin(player_counts[player_counts >= 200].index)]
 
 # # # Convert distance from cm to meters
 # # df["DISTANCE_METERS"] = df["distance"] / 100
@@ -68,14 +68,14 @@ from scipy.interpolate import make_interp_spline
 #     plt.figure(figsize=(10, 6))
 
 #     # Plot all players in gray with low opacity
-#     for player in df["PLAYER"].unique():
+#     for player in df[self.subject_col].unique():
 #         if player not in selected_players:
-#             player_data = df[df["PLAYER"] == player]
+#             player_data = df[df[self.subject_col] == player]
 #             plot_player_trajectory(player_data, column, color="gray", alpha=0.3)
 
 #     # Plot selected players in chosen colors
 #     for player in selected_players:
-#         player_data = df[df["PLAYER"] == player]
+#         player_data = df[df[self.subject_col] == player]
 #         color = colors[selected_players.index(player)]
 #         plot_player_trajectory(
 #             player_data, column, color=color, linewidth=4, label=player
@@ -145,20 +145,177 @@ class PlotConfig:
         self.color = color
         self.linewidth = linewidth
         self.alpha = alpha
+        self.subject_col = "PLAYER"
+
+
+# class TrajectoryPlotter:
+#     def __init__(self, config=PlotConfig()):
+#         self.config = config
+#         self.fig = None
+#         self.ani = None
+#         self.subject_col = "PLAYER"
+#         self.vlines = {"color": "black", "linestyle": "--"}
+#         self.hlines = {"color": "black", "linestyle": "--"}
+#         self.xlabel_params = {"fontsize": 18, "labelpad": 25}
+#         self.ylabel_params = {"fontsize": 18, "labelpad": 25}
+#         self.title_params = {"fontsize": 20}
+#         self.item_line_params = {"linewidth": 4}
+#         self.background_line_params = {"color": "gray", "alpha": 0.3}
+#         self.vlines_text_params = {
+#             "ha": "center",
+#             "va": "top",
+#             "color": "black",
+#             "fontsize": 12,
+#         }
+#         self.hlines_text_params = {
+#             "ha": "left",
+#             "va": "center",
+#             "color": "black",
+#             "fontsize": 12,
+#         }
+#         self.legend_params = {
+#             "loc": "center left",
+#             "bbox_to_anchor": (1, 0.5),
+#             "title": "Selected Players",
+#         }
+#         self.grid = True
+#         self.figsize = (10, 6)
+
+#     def create_smooth_line(self, x, y):
+#         # Replace NaN values with zero
+#         y = np.nan_to_num(y, nan=0.0)
+#         x_smooth = np.linspace(x.min(), x.max(), 300)
+#         spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
+#         y_smooth = spl(x_smooth)
+#         return x_smooth, y_smooth
+
+#     def plot_trajectory_data(
+#         self, data, column, color, alpha=1.0, linewidth=1.5, label=None
+#     ):
+#         x = np.array([bin.mid for bin in data["BIN"]])
+#         y = data[column].values
+
+#         # Create smooth lines using spline interpolation
+#         x_smooth, y_smooth = self.create_smooth_line(x, y)
+
+#         plt.plot(
+#             x_smooth,
+#             y_smooth,
+#             color=color,
+#             alpha=alpha,
+#             linewidth=linewidth,
+#             label=label,
+#         )
+
+#     def plot_trajectory(
+#         self,
+#         df,
+#         selected_items,
+#         colors,
+#         v_lines={},
+#         h_lines={},
+#         column="normalized_player_shots_relative_to_league",
+#         hide_yticks=False,
+#         xlabel="Distance from Basket (meters)",
+#         ylabel="Field Goals",
+#         title="Normalized Field Goals Relative to Rest of Euroleague 23/24",
+#     ):
+#         self.fig = plt.figure(self.figsize)
+
+#         # Plot all players in gray with low opacity
+#         for item in df[self.subject_col].unique():
+#             if item not in selected_items:
+#                 item_data = df[df[self.subject_col] == item]
+#                 self.plot_trajectory_data(
+#                     item_data, column, **self.background_line_params
+#                 )
+
+#         # Plot selected players in chosen colors
+#         for item in selected_items:
+#             item_data = df[df[self.subject_col] == item]
+#             color = colors[selected_items.index(item)]
+#             self.plot_trajectory_data(
+#                 item_data, column, color=color, **self.item_line_params, label=item
+#             )
+
+#         if v_lines:
+#             for vline, label in v_lines.items():
+#                 plt.axvline(x=vline, **self.vlines)
+#                 plt.text(vline, plt.ylim()[0] - 0.1, label, **self.vlines_text_params)
+
+#         if h_lines:
+#             for hline, label in h_lines.items():
+#                 plt.axhline(y=hline, **self.hlines)
+#                 plt.text(plt.xlim()[0] - 0.1, hline, label, **self.hlines_text_params)
+
+#         plt.xlabel(xlabel, **self.xlabel_params)
+#         plt.ylabel(ylabel, **self.ylabel_params)
+#         plt.title(title, **self.title_params)
+#         plt.grid(self.grid)
+
+#         if hide_yticks:
+#             plt.gca().yaxis.set_ticks([])
+
+#         plt.legend(**self.legend_params)
+#         plt.tight_layout()
+
+#         # Increase font size and adjust margins for x-axis labels
+#         plt.xticks(fontsize=12)
+#         plt.gca().xaxis.set_tick_params(labelsize=12)
+#         plt.subplots_adjust(bottom=0.2)
+
+#         plt.show()
 
 
 class TrajectoryPlotter:
-    def __init__(self, df, config=PlotConfig()):
-        self.df = df
-        self.config = config
+    def __init__(self, config=None):
+        default_params = {
+            "subject_col": "PLAYER",
+            "vlines": {"color": "black", "linestyle": "--"},
+            "hlines": {"color": "black", "linestyle": "--"},
+            "xlabel_params": {"fontsize": 18, "labelpad": 25},
+            "ylabel_params": {"fontsize": 18, "labelpad": 25},
+            "title_params": {"fontsize": 20},
+            "item_line_params": {"linewidth": 4},
+            "background_line_params": {"color": "gray", "alpha": 0.3},
+            "vlines_text_params": {
+                "ha": "center",
+                "va": "top",
+                "color": "black",
+                "fontsize": 12,
+            },
+            "hlines_text_params": {
+                "ha": "left",
+                "va": "center",
+                "color": "black",
+                "fontsize": 12,
+            },
+            "legend_params": {
+                "loc": "center left",
+                "bbox_to_anchor": (1, 0.5),
+                "title": "Selected Players",
+            },
+            "grid": True,
+            "figsize": (10, 6),
+        }
+
+        self.params = default_params
+        if config:
+            self.params.update(config)
+
         self.fig = None
         self.ani = None
 
+    def get_params(self):
+        return self.params
+
+    def set_params(self, **kwargs):
+        self.params.update(kwargs)
+
     def create_smooth_line(self, x, y):
-        # Replace NaN values with zero
         y = np.nan_to_num(y, nan=0.0)
         x_smooth = np.linspace(x.min(), x.max(), 300)
-        spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
+        spl = make_interp_spline(x, y, k=3)
         y_smooth = spl(x_smooth)
         return x_smooth, y_smooth
 
@@ -167,10 +324,7 @@ class TrajectoryPlotter:
     ):
         x = np.array([bin.mid for bin in data["BIN"]])
         y = data[column].values
-
-        # Create smooth lines using spline interpolation
         x_smooth, y_smooth = self.create_smooth_line(x, y)
-
         plt.plot(
             x_smooth,
             y_smooth,
@@ -182,7 +336,8 @@ class TrajectoryPlotter:
 
     def plot_trajectory(
         self,
-        selected_players,
+        df,
+        selected_items,
         colors,
         v_lines={},
         h_lines={},
@@ -192,60 +347,57 @@ class TrajectoryPlotter:
         ylabel="Field Goals",
         title="Normalized Field Goals Relative to Rest of Euroleague 23/24",
     ):
-        self.fig = plt.figure(figsize=(10, 6))
+        self.fig = plt.figure(figsize=self.params["figsize"])
 
-        # Plot all players in gray with low opacity
-        for player in self.df["PLAYER"].unique():
-            if player not in selected_players:
-                player_data = self.df[self.df["PLAYER"] == player]
-                self.plot_trajectory_data(player_data, column, color="gray", alpha=0.3)
+        for item in df[self.params["subject_col"]].unique():
+            if item not in selected_items:
+                item_data = df[df[self.params["subject_col"]] == item]
+                self.plot_trajectory_data(
+                    item_data, column, **self.params["background_line_params"]
+                )
 
-        # Plot selected players in chosen colors
-        for player in selected_players:
-            player_data = self.df[self.df["PLAYER"] == player]
-            color = colors[selected_players.index(player)]
+        for item in selected_items:
+            item_data = df[df[self.params["subject_col"]] == item]
+            color = colors[selected_items.index(item)]
             self.plot_trajectory_data(
-                player_data, column, color=color, linewidth=4, label=player
+                item_data,
+                column,
+                color=color,
+                **self.params["item_line_params"],
+                label=item,
             )
 
         if v_lines:
             for vline, label in v_lines.items():
-                plt.axvline(x=vline, color="black", linestyle="--")
+                plt.axvline(x=vline, **self.params["vlines"])
                 plt.text(
                     vline,
                     plt.ylim()[0] - 0.1,
                     label,
-                    ha="center",
-                    va="top",
-                    color="black",
-                    fontsize=12,
+                    **self.params["vlines_text_params"],
                 )
 
         if h_lines:
             for hline, label in h_lines.items():
-                plt.axhline(y=hline, color="black", linestyle="--")
+                plt.axhline(y=hline, **self.params["hlines"])
                 plt.text(
                     plt.xlim()[0] - 0.1,
                     hline,
                     label,
-                    ha="left",
-                    va="center",
-                    color="black",
-                    fontsize=12,
+                    **self.params["hlines_text_params"],
                 )
 
-        plt.xlabel(xlabel, fontsize=18, labelpad=25)
-        plt.ylabel(ylabel)
-        plt.title(title, fontsize=20)
-        plt.grid(True)
+        plt.xlabel(xlabel, **self.params["xlabel_params"])
+        plt.ylabel(ylabel, **self.params["ylabel_params"])
+        plt.title(title, **self.params["title_params"])
+        plt.grid(self.params["grid"])
 
         if hide_yticks:
             plt.gca().yaxis.set_ticks([])
 
-        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5), title="Selected Players")
+        plt.legend(**self.params["legend_params"])
         plt.tight_layout()
 
-        # Increase font size and adjust margins for x-axis labels
         plt.xticks(fontsize=12)
         plt.gca().xaxis.set_tick_params(labelsize=12)
         plt.subplots_adjust(bottom=0.2)
@@ -265,9 +417,9 @@ class TrajectoryPlotter:
     #     self.fig, ax = plt.subplots(figsize=(10, 6))
 
     #     # Plot all players in gray with low opacity
-    #     for player in self.df["PLAYER"].unique():
+    #     for player in df[self.subject_col].unique():
     #         if player not in selected_players:
-    #             player_data = self.df[self.df["PLAYER"] == player]
+    #             player_data = df[df[self.subject_col] == player]
     #             self.plot_trajectory_data(player_data, column, color="gray", alpha=0.3)
 
     #     lines = []
@@ -282,8 +434,8 @@ class TrajectoryPlotter:
     #         lines.append(line)
 
     #     def init():
-    #         ax.set_xlim(0, max(bin.mid for bin in self.df["BIN"]))
-    #         ax.set_ylim(self.df[column].min(), self.df[column].max())
+    #         ax.set_xlim(0, max(bin.mid for bin in df["BIN"]))
+    #         ax.set_ylim(df[column].min(), df[column].max())
     #         ax.set_xlabel(xlabel, fontsize=18, labelpad=25)
     #         ax.set_ylabel(ylabel)
     #         ax.set_title(title, fontsize=20)
@@ -294,7 +446,7 @@ class TrajectoryPlotter:
 
     #     def update(frame):
     #         for line, player in zip(lines, selected_players):
-    #             player_data = self.df[self.df["PLAYER"] == player]
+    #             player_data = df[df[self.subject_col] == player]
     #             x = np.array([bin.mid for bin in player_data["BIN"]])
     #             y = player_data[column].values
     #             x_smooth, y_smooth = self.create_smooth_line(x, y)
@@ -325,9 +477,9 @@ class TrajectoryPlotter:
         self.fig, ax = plt.subplots(figsize=(10, 6))
 
         # Plot all players in gray with low opacity
-        for player in self.df["PLAYER"].unique():
+        for player in df[self.subject_col].unique():
             if player not in selected_players:
-                player_data = self.df[self.df["PLAYER"] == player]
+                player_data = df[df[self.subject_col] == player]
                 self.plot_trajectory_data(player_data, column, color="gray", alpha=0.3)
 
         lines = []
@@ -342,8 +494,8 @@ class TrajectoryPlotter:
             lines.append(line)
 
         def init():
-            ax.set_xlim(0, max(bin.mid for bin in self.df["BIN"]))
-            ax.set_ylim(self.df[column].min() - 0.2, self.df[column].max() + 0.2)
+            ax.set_xlim(0, max(bin.mid for bin in df["BIN"]))
+            ax.set_ylim(df[column].min() - 0.2, df[column].max() + 0.2)
             ax.set_xlabel(xlabel, fontsize=18, labelpad=25)
             ax.set_ylabel(ylabel)
             ax.set_title(title, fontsize=20)
@@ -366,7 +518,7 @@ class TrajectoryPlotter:
 
             if h_lines:
                 for hline, label in h_lines.items():
-                    plt.axhline(y=hline, color="black", linestyle="--")
+                    plt.axhline(y=hline, **self.hlines)
                     plt.text(
                         ax.get_xlim()[0] - 0.1,
                         hline,
@@ -387,7 +539,7 @@ class TrajectoryPlotter:
             if player_index < len(selected_players):
                 line = lines[player_index]
                 player = selected_players[player_index]
-                player_data = self.df[self.df["PLAYER"] == player]
+                player_data = df[df[self.subject_col] == player]
                 x = np.array([bin.mid for bin in player_data["BIN"]])
                 y = player_data[column].values
                 x_smooth, y_smooth = self.create_smooth_line(x, y)
